9b600fbdaab04fa689f319ebea7e2a0df279d0a4
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-07-08 20:29:07 -0400

    Update

--- src/main/java/com/coderscampus/Assignment14/service/ChannelService.java
+++ src/main/java/com/coderscampus/Assignment14/service/ChannelService.java
@@ -11,6 +11,14 @@ public class ChannelService {
 
     private List<Channel> channels = new ArrayList<>();
 
+    public ChannelService() {
+        channels.add(new Channel(1L, "Business", new ArrayList<>()));
+        channels.add(new Channel(2L, "Cooking", new ArrayList<>()));
+        channels.add(new Channel(3L, "Gaming", new ArrayList<>()));
+        channels.add(new Channel(4L, "Sports", new ArrayList<>()));
+        channels.add(new Channel(5L, "Tech", new ArrayList<>()));
+    }
+
     public List<Channel> getAllChannels() {
         return channels;
     }
--- src/main/java/com/coderscampus/Assignment14/web/WelcomeController.java
+++ src/main/java/com/coderscampus/Assignment14/web/WelcomeController.java
@@ -1,17 +1,24 @@
 package com.coderscampus.Assignment14.web;
 
+import com.coderscampus.Assignment14.domain.Channel;
 import com.coderscampus.Assignment14.domain.User;
+import com.coderscampus.Assignment14.service.ChannelService;
 import com.coderscampus.Assignment14.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class WelcomeController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private ChannelService channelService;
 
 
     @PostMapping("/welcome")
@@ -23,7 +30,9 @@ public class WelcomeController {
     }
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(ModelMap model) {
+        List<Channel> channels = channelService.getAllChannels();
+        model.put("channels", channels);
         return "welcome";
     }
 
