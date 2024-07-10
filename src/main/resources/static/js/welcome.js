1b064926c188f9c268a08327930dccf740b63fa8
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-07-10 09:06:19 -0400

    Update

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -1,15 +1,23 @@
-let username = prompt("Enter your name");
+document.addEventListener("DOMContentLoaded", function() {
+    let username = sessionStorage.getItem('username')
+    if (!username) {
+        while (!username) {
+            username = prompt("Enter your username:")
+            if (username) {
+                sessionStorage.setItem("username", username)
+                fetch('/welcome', {
+                    method: 'POST',
 
-if (username) {
-    fetch('welcome/save)', {
-        method: 'POST',
-
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username})
-    })
-        .then(response => response.text())
-        .then(data => alert(data))
-        .catch(error => console.error(error));
-}
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify( {username: username} )
+                })
+                    .then(response => response.text())
+                    .catch(error => console.error(error))
+            } else {
+                alert("Username cannot be empty, please enter a valid username")
+            }
+        }
+    }
+})
