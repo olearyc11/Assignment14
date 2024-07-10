64faa89d79d16e7213934be3ac75b475142bfa08
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-07-10 12:10:56 -0400

    Update

--- src/main/java/com/coderscampus/Assignment14/service/MessageService.java
+++ src/main/java/com/coderscampus/Assignment14/service/MessageService.java
@@ -2,15 +2,22 @@ package com.coderscampus.Assignment14.service;
 
 import com.coderscampus.Assignment14.domain.Channel;
 import com.coderscampus.Assignment14.domain.Message;
+import com.coderscampus.Assignment14.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class MessageService {
 
+    @Autowired
+    MessageRepository messageRepository;
+
     private List<Message> messages = new ArrayList<>();
+
 
     public List<Message> getMessagesByChannelId(Channel channel) {
         List<Message> channelMessages = new ArrayList<>();
@@ -22,10 +29,11 @@ public class MessageService {
         return channelMessages;
     }
 
-    public void saveMessage(Message message) {
+    public List<Message> saveMessage(Message message) {
         Long messageId = (long) (messages.size() + 1);
         message.setMessageId(messageId);
         messages.add(message);
-    }
+        return messages;
+    } // maybe just return message?
 
 }
