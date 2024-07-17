bc477e77487e2c266bc858af74ed564e3c0fd742
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-07-17 15:42:23 -0400

    Update

--- src/main/java/com/coderscampus/Assignment14/web/ChannelController.java
+++ src/main/java/com/coderscampus/Assignment14/web/ChannelController.java
@@ -33,7 +33,9 @@ public class ChannelController {
         User user = userService.findUserById(userId);
         Channel channel = channelService.findChannelById(channelId);
         model.put("channel", channel);
+        System.out.println(channel.toString());
         model.put("user", user);
+        System.out.println(user.toString());
         return "channel";
     }
 
@@ -52,6 +54,7 @@ public class ChannelController {
         message.setUser(user);
         Channel channel = channelService.findChannelById(channelId);
         message.setChannelId(channel.getChannelId());
+        System.out.println(message.getMessageContent());
         return messageService.saveMessage(message);
     }
 
--- src/main/resources/static/js/channel.js
+++ src/main/resources/static/js/channel.js
@@ -1,25 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const messageBox = document.getElementById("messageBox");
-    const messageInput = document.getElementById("messageInput");
+document.addEventListener("DOMContentLoaded", function () {
+    const messageBox = document.getElementById("messageBox")
+    const messageInput = document.getElementById("messageInput")
     const username = sessionStorage.getItem("username")
+    const channelId = document.getElementById("channel_id").value
+    const userId = document.getElementById("user_id").value
+    const sendButton = document.getElementById("send_button")
     console.log(username)
+    console.log(channelId)
+    console.log(userId)
 
-
-    // messageBox.addEventListener("submit", function(event) {
-    //     event.preventDefault();
-    //
-    //     fetch('/chat', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify( {username: username, message: messageContent} )
-    //     })
-    //         .then(response => response.text())
-    //         .catch(error => console.error(error))
-    // })
-
-    messageBox.addEventListener("submit", function(event) {
+    messageBox.addEventListener("submit", function (event) {
         event.preventDefault()
         const messageContent = messageInput.value
 
@@ -30,7 +20,15 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             body: JSON.stringify({username: username, message: messageContent})
         }
-            .then(response => response.text())
+            .then(response => response.json())
+            .then(message => {
+                const messageElement = document.createElement("div")
+                // messageElement.textContent = `${message.username}: ${message.content}`
+                messageElement.textContent = `${message.user.username}: ${message.messageContent}`
+                console.log(messageElement)
+                document.getElementById("chatRoom").appendChild(messageElement)
+                messageInput.value = ""
+            })
             .catch(error => console.error(error))
     })
 
